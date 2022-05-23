740dbc2a539faa1934e8d7ae93cabf6486362c45
Jason Lee <70392664+jasonlee8600@users.noreply.github.com>
2022-05-23 13:12:42 -0700

    Update smart_pinball_agent.py

--- agents/smart_pinball_agent.py
+++ agents/smart_pinball_agent.py
@@ -81,23 +81,57 @@ def update():
     forward_step = 0.44
     turn_step = 1.0
 
-    # just to create endless while loop
-    looper = 0
 
-    while looper == 0:
+    # initialize "done" variable
+        # False when on road
+        # True when hits something
+    done = False
 
-        # initialize "done" variable
-            # False when on road
-            # True when hits something
-        done = False
+    # move forward while no obstacle in the way
+    while not done:
+        # initialize action array
+        action = np.array([0.0, 0.0])
 
-        # move forward while no obstacle in the way
-        while not done:
-            # initialize action array
+        print("Forward Step")
+        action += np.array([forward_step, 0.0])
+
+        v1 = action[0]
+        v2 = action[1]
+        # Limit radius of curvature
+        if v1 == 0 or abs(v2 / v1) > (min_rad + wheel_distance / 2.0) / (min_rad - wheel_distance / 2.0):
+            # adjust velocities evenly such that condition is fulfilled
+            delta_v = (v2 - v1) / 2 - wheel_distance / (4 * min_rad) * (v1 + v2)
+            v1 += delta_v
+            v2 -= delta_v
+
+        action[0] = v1
+        action[1] = v2
+
+        obs, reward, done, info = env.step(action)
+        print(done)
+        print("step_count = %s, reward=%.3f" % (env.unwrapped.step_count, reward))
+
+        env.render(args.cam_mode)
+
+            
+    # If we hit something
+    if done:
+        # undo the last action so we don't get stuck looping on a single error
+        undo_action(forward_step, 1, action, done)
+
+        # backup and rotate to the right for 30 steps
+        backup = 0
+        while backup != 30:
+
+            # reinitialize so no values from forward while loop
             action = np.array([0.0, 0.0])
 
-            print("Forward Step")
-            action += np.array([forward_step, 0.0])
+            # backup and rotate to the right
+            print("Backward Right Step")
+            action -= np.array([forward_step, 0.0])
+            action -= np.array([0.0, turn_step])
+
+            backup += 1
 
             v1 = action[0]
             v2 = action[1]
@@ -117,51 +151,17 @@ def update():
 
             env.render(args.cam_mode)
 
-            
-        # If we hit something
-        if done:
-            # undo the last action so we don't get stuck looping on a single error
-            undo_action(forward_step, 1, action, done)
+             # If we hit something while backing up
+            if done:
+                # undo last action
+                undo_action(forward_step, 0, action, done)
+                # break to first/forward while loop again
+                break
 
-            # backup and rotate to the right for 30 steps
-            backup = 0
-            while backup != 30:
-
-                # reinitialize so no values from forward while loop
-                action = np.array([0.0, 0.0])
-
-                # backup and rotate to the right
-                print("Backward Right Step")
-                action -= np.array([forward_step, 0.0])
-                action -= np.array([0.0, turn_step])
-
-                backup += 1
-
-                v1 = action[0]
-                v2 = action[1]
-                # Limit radius of curvature
-                if v1 == 0 or abs(v2 / v1) > (min_rad + wheel_distance / 2.0) / (min_rad - wheel_distance / 2.0):
-                    # adjust velocities evenly such that condition is fulfilled
-                    delta_v = (v2 - v1) / 2 - wheel_distance / (4 * min_rad) * (v1 + v2)
-                    v1 += delta_v
-                    v2 -= delta_v
-
-                action[0] = v1
-                action[1] = v2
-
-                obs, reward, done, info = env.step(action)
-                print(done)
-                print("step_count = %s, reward=%.3f" % (env.unwrapped.step_count, reward))
-
-                env.render(args.cam_mode)
-
-                # If we hit something while backing up
-                if done:
-                    # undo last action
-                    undo_action(forward_step, 0, action, done)
-                    # break to first/forward while loop again
-                    break
-
-update()
+# to create endless while loop
+looper = True
+while looper:
+    update()
 
 env.close()
+
