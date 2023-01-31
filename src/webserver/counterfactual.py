66ae4a952ee1d811bc1f4d1cd1dce159d2834124
Matt Elacqua <elacqua.matt@gmail.com>
2023-01-31 18:09:19 -0500

    added instructions for alex, added input change for aly

--- src/webserver/counterfactual.py
+++ src/webserver/counterfactual.py
@@ -440,35 +440,77 @@ def generate_klee_file(query_blob):
 # Generate the soid query
 def generate_soid_query(query_info):
     """
-    Will have to reach out to sam for this. I am under the impression of the following tasks:
+    generate the soid query
 
-    1. Describe the query. 
-        - This is done in the gui by the user, so assuming this is trivial. Maybe printing the formula in readable terms like:
-            - if factual
-                - Does the agent <specified question from query>, given the displayed scenario and the following concrete variables:
-                    - var 1
-                    - var 2
-                    - etc.
-            - if existential
-                - Does there exist a scenario in which the agent <specified question from query>, given the displayed symbolic scenario and the following concrete/symbolic variables:
-    2. Define the environment:
-        - Some of the learning state will be in here 
-            (all instances that deal with env or other agents: ex. intersection_empty, cars_waiting_to_enter)
-    3. Definte the state:
-        - Rest of learning state will be in here 
-            (all individually deducible ones: ex. approaching intersection)
-    4. Soid behavior 
-        - invoking if the agent will proceed from the klee file and seeing what the boolean answer is
 
     """
-    # return soid_query
-    return None
+    import soid 
+    from soid.soidlib import *
 
-# Invoke the soid and return the result
-def invoke_soid(soid_query, klee_file):
+    oracle = soid.Oracle()
+    klee_prefix = "src/webserver/soid_files/klee/"
+    make = klee_prefix + 'makefile'
+
+    query = soid.Soid('GUI Query', counterfactual.single) # We will use single for existential queries, many for (factual)
+
     """
-    This function will invoke soid given the path to the files and the soid queries.
-    Will likely need assistance from Sam on this. General outline is described in doc.
+    See the declare in https://gitlab.com/rose-yale/soid/-/blob/main/examples/car/queries/car.py for how this works.
+    Essentially this is what links soid -> the klee file. We are declaring only what we say is symbolic
+
+    E = {} is for environmental Variables
+    S = {} is for the states (so these are all of the symbolic variables we used for klee)
+    D(is P in example, but its old) = {} Is for the variable we want to test at the end result (which if you look at Klee.c is going to be our proceed_ret value.)
+
+    an outline of the declaration line is:
+        E[<Name we want to use in soid>] = soidlib.types.<***type (see below)***>(f'<name of variable in soid>', pp = None, raw = None)
+        
+        ***Sam is adding support for floats and doubles now, so put a placemarker variable like 
+
+        For pp and raw, pp we can ignore for now ( i think, set to none on all) and then raw = variable name in klee). You can leave the raw = None and pp = None and it will search for whatever name comes after the soidlib.types.
     """
-    # return soid_result
-    return None
+
+    @soid.register
+    def declare():
+        E = {"""Declare the environmental symbolic things, not really sure what from ours will go in because alot is static."""}
+        S = {"""This will include agent things"""}
+        P = {"""This will just have the proceed_ret value"""}
+
+    """
+    Now we have to register the actual constraints now that the variables are linked. See https://gitlab.com/rose-yale/soid/-/blob/main/examples/car/queries/causal_signaling.py. We just build the constraints. List of available keywords to us (Like And, Equal, etc are: https://gitlab.com/rose-yale/soid/-/blob/main/soid/soidlib/soidlib.py line 264. Getting Sam to add Or, LessThanEqualTo, GreaterThanEqualTo.) You can use filler names for now.
+
+    """
+    @soid.register
+    def environmental( E ):
+        """Here you will have to have something that generates this structure, like looping through the possible constraints that use variables E that we defined above. You may want something like this function to help:
+
+            def encode( E, config ):
+                formula = None
+                for thing in config:
+                    formula = And(Equal(E[ thing[0] ], thing[1]), formula)
+                return formula
+            
+            Treat config like a list of the variables in E and the constraints that come with them. You can do something similar in the following two as well.
+        """
+
+    @soid.register
+    def state( S ):
+        """
+        Here you will have to have something that generates this structure, like looping through the possible constraints that use variables S that we defined above
+        """
+
+    @soid.register
+    def behavior(E, S, D ):
+        """
+        Here you will have to have something that generates this structure, like looping through the possible constraints that use variables D that we defined above
+        """
+        return Equal(D.proceed_ret, True) # This will tell us if we ever move
+
+    
+    # invoke soid
+    (info, res, models, resources ) = soid.invoke(oracle, make, query)
+    # Ignore info
+    # Res is the result
+    # Models is empty or models[0] is the model
+    # resources is the timing of the query
+    return res, models, resources
+
