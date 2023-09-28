c0d252559f462a3e6e9a16deb642f45be1befef8
Samuel Judson <sam@sjudson.com>
2023-09-28 11:56:36 -0400

    Fix model choice dropdown.

--- src/webserver/web-gui/src/agents/ModelChoice.js
+++ src/webserver/web-gui/src/agents/ModelChoice.js
@@ -43,20 +43,21 @@ class ModelChoice extends React.Component {
     return( 
       <div className="ModelChoice">
         <h4> Decision Model: </h4>
-        <Select
-          options={options} // Options to display in the dropdown
-          defaultValue={{value: 'good_agent', label: 'Ideal Agent (Hard-Coded Behavior)'}} // Options to display in the dropdown
-          theme={theme => ({
-            ...theme,
-            borderRadius: 0,
-            colors: {
-              ...theme.colors,
-              primary25: '#DDCC77',
-              primary: '#999933',
-            },
-          })}
-          onChange= {new_option => {this.handleChoice(new_option.value)}}
-          />
+        <div className="ModelSelector">
+          <Select
+            options={options} // Options to display in the dropdown
+            defaultValue={{value: 'good_agent', label: 'Ideal Agent (Hard-Coded Behavior)'}} // Options to display in the dropdown
+            theme={theme => ({
+              ...theme,
+              borderRadius: 0,
+              colors: {
+                ...theme.colors,
+                primary25: '#DDCC77',
+                primary: '#999933',
+              },
+            })}
+            onChange= {new_option => {this.handleChoice(new_option.value)}} />
+          </div>
       </div>);
   }
 }
